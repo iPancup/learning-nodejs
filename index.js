378c34cd73e437017f547641a1e4c34cedf277f0
iPancup <ivanyusuf642@gmail.com>
2019-09-04 21:45:59 +0700

    changee

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-var http;
+var http = require('http');
+var port = process.env.PORT || 9000;
 
 http.createServer(function(req,res){
+    res.writeHead(200,{'content-type':'text/plain'});
     res.end('{ "success:true", "data:Index", "message:Hello World", "code:200"}');
-})
-
-app.listen(process.env.PORT || 3000, function(){
-    console.log('Your node js server is running');
-});
+}).listen(port,'127.0.0.1');
