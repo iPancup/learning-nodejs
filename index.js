dfcb455ae5f5e96c2aac64a632791eb541d8fc27
iPancup <ivanyusuf642@gmail.com>
2019-09-04 21:58:49 +0700

    http bro

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-var express = require('express');
-var app = express();
+var http = require('http');
 
-app.get('/', function(req,res) {
-    res.send('{"success:true", "data:Index", "message:Hello World", "code:200"}');
-})
-
-app.listen(process.env.PORT || 3000, function(){
-    console.log('Your node js server is running');
-});
+http.createServer('/', function(req,res,next) {
+    res.writeHead(200, {'Content-Type':'text/plain'});
+    res.end('{"success:true", "data:Index", "message:Hello World", "code:200"}');
+}).listen(process.env.PORT || 3000);
